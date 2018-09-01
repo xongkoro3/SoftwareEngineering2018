02e75afb47d6af11d933323239b9fe7bfb0dbfbd
Shuai He <hs.heshuai@gmail.com>
2018-09-01 07:22:16 -0400

    Main method for Q3

--- src/edu/nd/se2018/homework/hwk1/Question3.java
+++ src/edu/nd/se2018/homework/hwk1/Question3.java
@@ -6,4 +6,10 @@ public class Question3 {
     public int getMirrorCount(int[] numbers){
 		return 0;	
 	}
+    
+	public static void main(String[] args) {
+		Question3 q = new Question3();
+		int res = q.getMirrorCount(new int[] {1,2,4,5,3,2,1});
+		System.out.println(res);
+	}
 }
