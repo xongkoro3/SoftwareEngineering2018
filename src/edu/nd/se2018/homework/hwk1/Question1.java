71e860612928d863738c02b2034908b4260e3f0b
Shuai He <hs.heshuai@gmail.com>
2018-08-31 23:53:29 -0400

    Q1 and Q2 updated

--- src/edu/nd/se2018/homework/hwk1/Question1.java
+++ src/edu/nd/se2018/homework/hwk1/Question1.java
@@ -1,10 +1,26 @@
 package edu.nd.se2018.homework.hwk1;
 
+import java.util.*;
+
 public class Question1 {
 		
 	public Question1(){}
 	
 	public int getSumWithoutDuplicates(int[] numbers){
-		return 0;	
+		HashSet<Integer> h = new HashSet<Integer>();
+	    for (int i = 0; i < numbers.length; i++){
+	    	if (!h.contains(i)){
+	    		h.add(numbers[i]);
+	    	}
+	    }
+        Integer integerSum = h.stream().mapToInt(Integer::intValue).sum();
+	    return integerSum;
+	    
+	}
+	
+	public static void main(String[] args) {
+		Question1 q = new Question1();
+		int res = q.getSumWithoutDuplicates(new int[] {1,2,2,2,2,2,2});
+		System.out.println(res);
 	}
 }
--- src/edu/nd/se2018/homework/hwk1/Question2.java
+++ src/edu/nd/se2018/homework/hwk1/Question2.java
@@ -1,10 +1,61 @@
 package edu.nd.se2018.homework.hwk1;
 
+import java.util.*;
+
+
 public class Question2 {
 
 	public Question2(){}
 	
 	public String getMostFrequentWord(String input, String stopwords){
-		return "";
+		HashMap<String, Integer> h = new HashMap<String, Integer>();
+		String[] StopWordSplit = stopwords.split(" ");
+		List<String> stopList = new ArrayList<String>(Arrays.asList(StopWordSplit));
+		String[] inputSplit = input.split(" ");
+		List<String> inputList = new ArrayList<String>(Arrays.asList(inputSplit));
+
+        for (String s: stopList) {
+            if (inputList.contains(s)) {
+            	inputList.removeAll(Collections.singleton(s));//remove stop word
+            }
+        }
+        
+        for (String ss: inputList) {
+        	if (!h.containsKey(ss)) {
+        		h.put(ss, 1);
+        	}
+        	else {
+        		h.put(ss, h.get(ss) + 1);
+        	}
+//        	System.out.println(ss);
+        }
+        
+        Integer maxValue = 0;
+        List<String> maxKeys = new ArrayList<String>();
+        for (HashMap.Entry<String, Integer> entry : h.entrySet()) {
+          if (maxValue == 0 || maxValue.equals(entry.getValue())) {
+            maxValue = entry.getValue();
+            maxKeys.add(entry.getKey());
+          } else if (entry.getValue() > maxValue) {
+            maxValue = entry.getValue();
+            maxKeys.clear();
+            maxKeys.add(entry.getKey());
+          }
+        }
+ 
+        if (maxKeys.size()!= 1) {
+        	return null;
+        }
+        
+		return maxKeys.get(0);
+	}
+	
+	public static void main(String[] args) {
+		Question2 q = new Question2();
+		String inputString3 = "giraffe elephant of giraffe the tiger";
+		String stopWords = "and a hes the of up but with";
+		
+		String res = q.getMostFrequentWord(inputString3, stopWords);
+		System.out.println(res);
 	}
 }
